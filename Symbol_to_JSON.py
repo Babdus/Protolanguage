7b44d71d97738fee8e78d919eea9930066040c81
Babdus <babdusi@gmail.com>
2017-05-20 00:22:47 +0400

    Add real features and some symbols

--- Symbol_to_JSON.py
+++ Symbol_to_JSON.py
@@ -2,16 +2,17 @@ import sys
 import json
 
 def main():
-    with open('Features.txt') as f:
-        feature_names = [feature.strip() for feature in f]
+    with open('Features.txt') as infile:
+        feature_names = [feature.strip() for feature in infile]
     symbol_features = {}
-    with open('Symbol_Feature_Map.txt') as f:
-        for line in f:
+    with open('Symbol_Feature_Map.txt') as infile:
+        for line in infile:
             line = line.split()
-            features = dict(zip(feature_names, line[1:]))
+            bool_features = [bool(int(digit)) for digit in line[1:]]
+            features = dict(zip(feature_names, bool_features))
             symbol_features[line[0]] = features
-    symbols_json = json.dumps(symbol_features)
-    print symbols_json
+    with open('Symbols.json', 'w') as outfile:
+        json.dump(symbol_features, outfile)
 
 if __name__ == "__main__":
     main()
