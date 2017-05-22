aed50ed84b6d8ee2a45eb98516d50977be3cc3fd
Babdus <babdusi@gmail.com>
2017-05-22 15:30:05 +0400

    Add some features

--- Symbol_to_JSON.py
+++ Symbol_to_JSON.py
@@ -8,11 +8,17 @@ def main():
     with open('Symbol_Feature_Map.txt') as infile:
         for line in infile:
             line = line.split()
+            if line[0] == 'end':
+                break
             bool_features = [bool(int(digit)) for digit in line[1:]]
             features = dict(zip(feature_names, bool_features))
             symbol_features[line[0]] = features
     with open('Symbols.json', 'w') as outfile:
         json.dump(symbol_features, outfile)
+    for symbol in symbol_features:
+        print symbol
+        for feature in symbol_features[symbol]:
+            print '\t', feature, '-', int(symbol_features[symbol][feature])
 
 if __name__ == "__main__":
     main()
