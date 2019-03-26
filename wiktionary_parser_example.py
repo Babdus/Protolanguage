631eb4cd784d3786e30e28670c5db83bd6ea97eb
Babdus <babdusi@gmail.com>
2019-03-26 04:46:59 +0400

    Add second bits of wiktionary parser

--- wiktionary_parser_example.py
+++ wiktionary_parser_example.py
@@ -1,7 +1,4 @@
-import urllib.request
 import sys
-from lxml import html
-import re
 import time
 from bs4 import BeautifulSoup
 import requests
@@ -10,7 +7,6 @@ def main(argv):
     start_time = time.time()
     main_lang = argv[0]
     word = argv[1]
-    translation_lang = argv[2]
     url = f'https://{main_lang}.wiktionary.org/wiki/{word}'
 
     html = requests.get(url).content
@@ -19,38 +15,51 @@ def main(argv):
 
     dictionary = {}
 
-    langs = argv[2:]
+    if len(argv) > 2:
+        langs = argv[2:]
+    else:
+        langs = []
+        with open('language_list') as fp:
+            for line in fp:
+                langs.append(line.split()[0])
 
     for lang in langs:
 
         translation_tag = translations_table.find('span', lang=lang)
+        if translation_tag is None:
+            continue
         href = translation_tag.find('a').get('href')
+        if href is None:
+            continue
         translation = translation_tag.text
 
         url_2 = f'https://{main_lang}.wiktionary.org{href}'
         html_2 = requests.get(url_2).content
         soup_2 = BeautifulSoup(html_2, 'lxml')
-        anchor = href.split('#')[1]
-        heading = soup_2.find('span', id=anchor).parent
+        href_list = href.split('#')
+        if len(href_list) < 2:
+            continue
+        anchor = href_list[1]
+        heading_span = soup_2.find('span', id=anchor)
+        if heading_span is None:
+            continue
+        heading = heading_span.parent
 
         transcriptions = []
 
         for next_heading in heading.next_siblings:
             if next_heading.name == 'h2':
                 break
-            if type(next_heading).__name__ == 'NavigableString':
-                continue
-            transcription_tags = next_heading.findAll('span', {"class": "IPA"})
-            for transcription_tag in transcription_tags:
-                transcription = transcription_tag.text
-                transcription = transcription.strip('][/')
-                if transcription[0] != '-':
-                    transcriptions.append(transcription)
+            if type(next_heading).__name__ == 'Tag':
+                transcription_tags = next_heading.findAll('span', {"class": "IPA"})
+                for transcription_tag in transcription_tags:
+                    transcription = transcription_tag.text
+                    transcription = transcription.strip('][/')
+                    if transcription[0] != '-':
+                        transcriptions.append(transcription)
 
         dictionary[translation] = transcriptions
-        print(translation, transcriptions)
-
-    print(dictionary)
+        print(f'\033[33;1m{anchor}\033[0m: {translation} {transcriptions}')
 
 
 if __name__ == "__main__":
