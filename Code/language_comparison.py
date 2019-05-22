16d172c667a6d4a143785a082724e4c597c9dc9e
Babdus <babdusi@gmail.com>
2019-05-22 17:25:33 +0400

    Add test 77 languages to language comparison.py

--- Code/language_comparison.py
+++ Code/language_comparison.py
@@ -4,6 +4,14 @@ import pandas as pd
 from IPA.IPAString import IPAString as Istr
 from IPA.IPAStringComparison import IPAStringComparison as Istcom
 
+test_langs = ['af', 'ar', 'as', 'az', 'ba', 'be', 'bg', 'bn', 'bo', 'br', 'ca',
+              'cs', 'cy', 'da', 'de', 'el', 'en', 'es', 'fa', 'fi', 'fo', 'fr',
+              'fy', 'ga', 'gd', 'gl', 'gv', 'he', 'hi', 'hu', 'hy', 'id', 'is',
+              'it', 'ja', 'ka', 'km', 'ko', 'ku', 'lb', 'lo', 'lt', 'lv', 'mk',
+              'mn', 'ms', 'mt', 'my', 'nl', 'nn', 'ny', 'oc', 'pl', 'ps', 'pt',
+              'ro', 'ru', 'sa', 'se', 'sk', 'sl', 'sq', 'sv', 'te', 'tg', 'th',
+              'tk', 'tl', 'tr', 'ug', 'uk', 'ur', 'vi', 'xh', 'yi', 'za', 'zu']
+
 def calculate_distance(df, lang_1, lang_2, output_file):
     lang_1_words = df[lang_1]
     lang_2_words = df[lang_2]
@@ -26,24 +34,26 @@ def main(argv):
     df = pd.io.parsers.read_csv(argv[0],index_col=0).fillna('')
 
     # for col1 in df.columns:
-    matrix = []
-    for i, col1 in enumerate(df.columns):
-        row = []
-        for j, col2 in enumerate(df.columns):
-            if i <= j:
-                row.append('')
-                continue
-            print(col1, col2)
-            distances, output = calculate_distance(df, col1, col2, None)
-            if len(distances) > 20:
+    with open(argv[1], 'w') as out:
+        # matrix = []
+        for i, col1 in enumerate(test_langs):
+            out.write(col1)
+            # row = []
+            for j, col2 in enumerate(test_langs):
+                if i <= j:
+                    out.write(',')
+                    # row.append('')
+                    continue
+                print(col1, col2)
+                distances, output = calculate_distance(df, col1, col2, None)
                 # out = list(map(lambda x: f'{x[0]}: {x[1]} {x[2]}', output))
-                row.append(f'{sum(distances)/len(distances)*100000//1/100000}')
-            else:
-                row.append('-')
-        matrix.append(row)
+                out.write(f',{sum(distances)/len(distances)*100000//1/100000}')
+                # row.append(f'{sum(distances)/len(distances)*100000//1/100000}')
+            out.write('\n')
+            # matrix.append(row)
 
-    df2 = pd.DataFrame(matrix, index=df.columns, columns=df.columns)
-    df2.to_csv(argv[1])
+    # df2 = pd.DataFrame(matrix, index=test_langs, columns=test_langs)
+    # df2.to_csv(argv[1])
 
     end = time()
     print(((end-start)*1000//1)/1000, 'seconds')
