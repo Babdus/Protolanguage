ee48bec39fcd63eeb18b0d2da71c3a6c0f70ed48
Babdus <babdusi@gmail.com>
2019-05-23 03:40:46 +0400

    Create tree v1

--- Code/generate_tree.py
+++ Code/generate_tree.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+from distance_dict_to_tree_dict import distance_to_tree
 
 def main(argv):
     d = pd.io.parsers.read_csv(argv[0],index_col=0).fillna('')
@@ -20,17 +21,18 @@ def main(argv):
     delta = {}
 
     n = d.shape[0]
-    while n > 2:
+    while n > 1:
 
+        print('\033[34;1md\033[0m')
         print(d)
 
         #1 Compute Q matrix
         for a in d.columns:
             for b in d.columns:
-                print(a, b)
                 if a != b:
                     Q[a][b] = (n-2)*d[a][b] - sum(d[a][k] for k in d.columns) - sum(d[b][k] for k in d.columns)
 
+        print('\033[33;1mQ\033[0m')
         print(Q)
 
         #2 Find pair with minimal value in Q
@@ -38,9 +40,13 @@ def main(argv):
 
         #3 Create parent node for minimal found pair and assign edge lengths
         ab = a+b
-        delta[(a, ab)] = 0.5*d[a][b] + 1/(2*(n-2)) * (sum(d[a][k] for k in d.columns) - sum(d[b][k] for k in d.columns))
+        if n == 2:
+            delta[(a, ab)] = 0.5*d[a][b]
+        else:
+            delta[(a, ab)] = 0.5*d[a][b] + 1/(2*(n-2)) * (sum(d[a][k] for k in d.columns) - sum(d[b][k] for k in d.columns))
         delta[(b, ab)] = d[a][b] - delta[(a, ab)]
 
+        print('\033[32;1mDelta\033[0m')
         print(delta)
 
         #4 Add row and column to d for new node
@@ -50,13 +56,15 @@ def main(argv):
         matrix.append([0]*(n+1))
         d = pd.DataFrame(matrix, columns=d.columns, index=d.columns)
 
+        print('\033[34;1md\033[0m')
         print(d)
-        print(d.index)
 
         #5 Define distances to other nodes from new one
         for k in d.columns:
-            d[k][ab] = d[ab][k] = 0.5 * (d[a][k] + d[b][k] - d[a][b])
+            if k != ab:
+                d[k][ab] = d[ab][k] = 0.5 * (d[a][k] + d[b][k] - d[a][b])
 
+        print('\033[34;1md\033[0m')
         print(d)
 
         #6 Remove columns and rows for already parented nodes
@@ -65,15 +73,19 @@ def main(argv):
 
         Q = pd.DataFrame(columns=d.columns, index=d.columns)
 
-        print(d)
+        print('\033[33;1mQ\033[0m')
+        print(Q)
 
-        print('\033[33;1mHERE!!!\033[0m')
+        print('\033[34;1md\033[0m')
+        print(d)
 
         n = d.shape[0]
 
-        print(n)
+        print('\033[35mn\033[0m: ', n)
 
     print(delta)
+
+    distance_to_tree(delta)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
